98492a09038a0b7d78e62f029904e24d73faff45
fleskara <nikola9100@gmail.com>
2022-05-12 22:30:40 +0200

    (feat) Created planet gravity

---
+++ Assets/Scripts/ObjectGravity.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGravity : MonoBehaviour
+{
+    [SerializeField] float gravityForce = 1500f;
+    [SerializeField] Vector3 planetCenter;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = transform.GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 gravityDirection = (planetCenter -transform.position).normalized;
+        rigidbody.AddForce(gravityDirection * (gravityForce * Time.fixedDeltaTime), ForceMode.Acceleration);
+
+        Quaternion rotateUp = Quaternion.FromToRotation(transform.up, -gravityDirection);
+        //Quaternion newRotation = Quaternion.Slerp(rigidbody.rotation, rotateUp * rigidbody.rotation, Time.fixedDeltaTime * 5f);
+        Quaternion newRotation = rigidbody.rotation * rotateUp;
+        rigidbody.MoveRotation(newRotation);
+    }
+}
